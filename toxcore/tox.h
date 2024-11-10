934a8301113e6c6cb2bf1fb6791135cade908c72
Robin Linden <dev@robinlinden.eu>
2024-11-11 00:54:53 +0100

    chore: Release 0.2.20

--- toxcore/tox.h
+++ toxcore/tox.h
@@ -148,7 +148,7 @@ uint32_t tox_version_minor(void);
  * Incremented when bugfixes are applied without changing any functionality or
  * API or ABI.
  */
-#define TOX_VERSION_PATCH              19
+#define TOX_VERSION_PATCH              20
 
 uint32_t tox_version_patch(void);
 
